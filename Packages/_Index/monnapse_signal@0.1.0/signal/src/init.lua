6d50459a55f78d0111bab9913f2d7c20a499d7c8
Monnapse <masonshuber@gmail.com>
2023-08-02 22:42:38 -0600

    Update init.lua

--- Packages/_Index/monnapse_signal@0.1.0/signal/src/init.lua
+++ Packages/_Index/monnapse_signal@0.1.0/signal/src/init.lua
@@ -6,26 +6,58 @@
 local signal = {}
 signal.__index = signal
 
+--[=[
+    @class signal
+    @client
+    @server
+    Send signals over scripts easily
+]=]
 export type signal = {}
 
+--[=[
+    Create a new signal
+
+    @client
+    @server
+]=]
 function signal.new(): signal
     local self = setmetatable({}, signal)
     self.CallbackEvents = {}
     return self
 end
 
-function signal:Fire(...)
+--[=[
+    Fire an event to a signal
+
+    @client
+    @server
+
+    @param ... any -- Parameters that will be sent to the signal
+]=]
+function signal:Fire(...: any)
     for index, CallbackEvent in pairs(self.CallbackEvents) do
         CallbackEvent(...)
     end
 end
 
---// Connect to the signal to get callbacks
+--[=[
+    Connect to the signal to get callbacks
+
+    @client
+    @server
+
+    @param CallbackEvent function -- The event that will be called when the signal is fired
+]=]
 function signal:Connect(CallbackEvent)
     table.insert(self.CallbackEvents,CallbackEvent)
 end
 
---// Destroy the signal
+--[=[
+    Destroy the signal
+
+    @client
+    @server
+]=]
 function signal:Destroy()
     setmetatable(self,nil)
 end
